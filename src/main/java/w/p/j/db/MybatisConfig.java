99920333df0b2046e0a9daa1669f2d91ea84f73c
wupeiji <wpjlovehome@gmail.com>
2015-10-04 23:34:49 +0800

    解决了配置文件问题，剔除了xml的配置方法

--- src/main/java/w/p/j/db/MybatisConfig.java
+++ src/main/java/w/p/j/db/MybatisConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 - 9 - 28  11 : 44 :$second.second
+ * Copyright (c) 2015 - 10 - 3  11 : 13 :$second.second
  * @author wupeiji It will be
  * @Email wpjlovehome@gmail.com
  */
@@ -7,6 +7,8 @@
 package w.p.j.db;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.github.pagehelper.PageHelper;
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -15,7 +17,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.StandardEnvironment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -26,7 +30,6 @@ import org.springframework.util.ClassUtils;
 import tk.mybatis.spring.mapper.MapperScannerConfigurer;
 import w.p.j.util.MyMapper;
 
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -37,85 +40,91 @@ import java.util.Properties;
  */
 @Configuration
 @EnableAutoConfiguration(exclude = {DruidDataSourceEntity.class})
-//@MapperScan(basePackages = "w.p.j.dao",markerInterface = MyMapper.class)
-@ImportResource("classpath:applicationContext.xml")
 public class MybatisConfig {
-//    private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
-//    @Autowired
-//    private DruidDataSourceEntity druidDataSourceEntity;
-//    @Bean
-//    public DataSource dataSource() {
-//        logger.info("------------druidDataSourceEntity------" + druidDataSourceEntity);
-//        //加载配置文件属性
-//        DruidDataSource dataSource = new DruidDataSource();
-//        dataSource.setDriverClassName(druidDataSourceEntity.getDriverClassName());
-//        dataSource.setUsername(druidDataSourceEntity.getUsername());
-//        dataSource.setPassword(druidDataSourceEntity.getPassword());
-//        dataSource.setUrl(druidDataSourceEntity.getUrl());
-//        dataSource.setMaxActive(druidDataSourceEntity.getMaxActive());
-//        dataSource.setValidationQuery(druidDataSourceEntity.getValidationQuery());
-//        dataSource.setTestOnBorrow(druidDataSourceEntity.isTestOnBorrow());
-//        dataSource.setTestOnReturn(druidDataSourceEntity.isTestOnReturn());
-//        dataSource.setTestWhileIdle(druidDataSourceEntity.isTestWhileIdle());
-//        dataSource.setTimeBetweenEvictionRunsMillis(druidDataSourceEntity.getTimeBetweenEvictionRunsMillis());
-//        dataSource.setMinEvictableIdleTimeMillis(druidDataSourceEntity.getMinEictableIdleTimeMillis());
-//        dataSource.setPoolPreparedStatements(druidDataSourceEntity.isPoolPreparedStatements());
-//        dataSource.setMaxOpenPreparedStatements(druidDataSourceEntity.getMaxOpenPreparedStatements());
-//        try {
-//            dataSource.setFilters(druidDataSourceEntity.getFilters());
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return dataSource;
-//    }
-//
-//    public Resource[] getResource(String basePackage, String pattern) throws IOException {
-//        String packageSearchPath = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX
-//                + ClassUtils.convertClassNameToResourcePath(new StandardEnvironment()
-//                .resolveRequiredPlaceholders(basePackage)) + "/" + pattern;
-//        Resource[] resources = new PathMatchingResourcePatternResolver().getResources(packageSearchPath);
-//        return resources;
-//    }
-//
-//    @Bean
-//    public SqlSessionFactory sqlSessionFactory() throws Exception {
-//        logger.info("------> sqlSessionFactory");
-//        final SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
-//        sqlSessionFactory.setDataSource(dataSource());
-//        sqlSessionFactory.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
-//        sqlSessionFactory.setFailFast(true);
-//        sqlSessionFactory.setMapperLocations(getResource("mapper", "**/*.xml"));
-//        //配置插件
-//
-//        return sqlSessionFactory.getObject();
-//    }
-//
-//
-//    @Bean
-//    public DataSourceTransactionManager transactionManager() {
-//        logger.info("> transactionManager");
-//        return new DataSourceTransactionManager(dataSource());
-//    }
-//
-//    @Bean
-//    @Scope(value = "prototype")
-//    public SqlSessionTemplate sqlSessionTemplate() {
-//
-//        try {
-//            logger.info("SqlSessionTemplate---");
-//            return new SqlSessionTemplate(sqlSessionFactory());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        logger.info("---sqlSesson----","空");
-//        return null;
-//    }
-//
-//    @PostConstruct
-//    public void postConstruct() {
-//        logger.info("jdbc.settings={}", druidDataSourceEntity);
-//        logger.info("sqlSession",sqlSessionTemplate());
-//
-//    }
+    private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+
+    @Autowired
+    private DruidDataSourceEntity druidDataSourceEntity;
+
+    @Bean
+    public DataSource dataSource() {
+        logger.info("------------druidDataSourceEntity------" + druidDataSourceEntity);
+        //加载配置文件属性
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(druidDataSourceEntity.getDriverClassName());
+        dataSource.setUsername(druidDataSourceEntity.getUsername());
+        dataSource.setPassword(druidDataSourceEntity.getPassword());
+        dataSource.setUrl(druidDataSourceEntity.getUrl());
+        dataSource.setMaxActive(druidDataSourceEntity.getMaxActive());
+        dataSource.setValidationQuery(druidDataSourceEntity.getValidationQuery());
+        dataSource.setTestOnBorrow(druidDataSourceEntity.isTestOnBorrow());
+        dataSource.setTestOnReturn(druidDataSourceEntity.isTestOnReturn());
+        dataSource.setTestWhileIdle(druidDataSourceEntity.isTestWhileIdle());
+        dataSource.setTimeBetweenEvictionRunsMillis(druidDataSourceEntity.getTimeBetweenEvictionRunsMillis());
+        dataSource.setMinEvictableIdleTimeMillis(druidDataSourceEntity.getMinEictableIdleTimeMillis());
+        dataSource.setPoolPreparedStatements(druidDataSourceEntity.isPoolPreparedStatements());
+        dataSource.setMaxOpenPreparedStatements(druidDataSourceEntity.getMaxOpenPreparedStatements());
+        try {
+            dataSource.setFilters(druidDataSourceEntity.getFilters());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
+
+    public Resource[] getResource(String basePackage, String pattern) throws IOException {
+        String packageSearchPath = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX
+                + ClassUtils.convertClassNameToResourcePath(new StandardEnvironment()
+                .resolveRequiredPlaceholders(basePackage)) + "/" + pattern;
+        Resource[] resources = new PathMatchingResourcePatternResolver().getResources(packageSearchPath);
+        return resources;
+    }
+
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        logger.info("------> sqlSessionFactory");
+        final SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource());
+        sqlSessionFactory.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
+        Interceptor[] interceptors = new Interceptor[1];
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("dialect", "mysql");
+        properties.setProperty("reasonable", "true");
+        pageHelper.setProperties(properties);
+        interceptors[0] = pageHelper;
+        sqlSessionFactory.setPlugins(interceptors);
+        sqlSessionFactory.setFailFast(true);
+        sqlSessionFactory.setMapperLocations(getResource("mapper", "**/*.xml"));
+        //配置插件
+
+        return sqlSessionFactory.getObject();
+    }
+
+
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        logger.info("> transactionManager");
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    @Scope(value = "prototype")//线程安全
+    public SqlSessionTemplate sqlSessionTemplate() {
+
+        try {
+            logger.info("SqlSessionTemplate---");
+            return new SqlSessionTemplate(sqlSessionFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        logger.info("---sqlSesson----", "null");
+        return null;
+    }
+
+
 
 }
